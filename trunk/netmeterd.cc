6fa901386206467d334fd1df18eb91cca843c663
skyhusker <skyhusker@fcc34d41-85d9-0310-a7b1-a7f9df185352>
2004-05-14 23:07:03 +0000

    Trying to code signal handlers

--- trunk/netmeterd.cc
+++ trunk/netmeterd.cc
@@ -1,7 +1,11 @@
 //$Id$
 
+#include <vector>
+#include <string>
 #include <iostream>
 #include <getopt.h>
+#include <signal.h>
+
 #include "counter.h"
 #include "iface.h"
 
@@ -11,21 +15,50 @@ void help()
   << "Copyright: Alberto Garcia Hierro and Ayoze Roberto "
   << "Bernardo" << std::endl
   << "Usage: netmeterd [options]" << std::endl
-  << "-i:    Specifies the interfaces (interface1,interface2,...)"
+  << "-i:   Specifies the interfaces (interface1,interface2,...)"
   << std::endl
-  << "-t:    Specifies the time in secodns between counter updates"
+  << "-t:   Specifies the time in secodns between counter updates"
   << std::endl
-  << "-f:    Run on foreground (don't daemonize)" << std::endl;
+  << "-f:   Run on foreground (don't daemonize)" << std::endl;
 
 }
+
+std::vector <std::string> getNames (const std::string &interfaces)
+{
+  std::vector <std::string> ret;
+  std::string buf="";
+  
+  for (unsigned int i=0;i<interfaces.size();i++)
+    if (interfaces.at(i) != ',')
+    {
+      buf += interfaces.at(i);
+    }
+    else
+    {
+      ret.push_back(buf);
+      buf="";
+    }
+  //We need to push the last name too
+  ret.push_back(buf);
+  return ret;
+}
+
+void quit(){
+  std::cout << "Morire con dignidad" << std::endl;
+}
+
+
 
 int main(int argc, char *argv[])
 {
   int time=1,ch;
   bool wantsdaemon=true;
-  std::string interfaces;
-  
+  std::string interfaces="";
+  std::vector <iface> ifaces;
+  std::vector <std::string> names; 
+  struct sigaction signal_action;
 
+  //Parse command line
   while ((ch = getopt (argc, argv, "i:t:fh")) != -1)
     switch (ch)
     {
@@ -48,6 +81,31 @@ int main(int argc, char *argv[])
         return 1;
       }
 
-  std::cout << time << " " << interfaces << std::endl;
+  if (interfaces=="")
+  {
+    std::cout << "Error: you must specify one interface at least."
+    << std::endl;
+    return 1;
+  }
+  
+  names = getNames(interfaces);
+  //Create the ifaces
+  for (unsigned int i=0;i<names.size();i++)
+    ifaces.push_back(iface(names[i]));
+  
+  //TODO: Check write perms
+  
+  //Signal handlers
+  sigaddset(&signal_action.sa_mask, SIGTERM);
+  signal_action.sa_handler = quit;
+  sigaction(SIGTERM, &signal_action, 0);
+          
+
+  
+  if(wantsdaemon)
+    daemon(0,0);
+   
+  while (true);
+
   return 0;
 }
