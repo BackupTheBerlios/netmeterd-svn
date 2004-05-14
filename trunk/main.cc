832776baf32782a024da969ac6edbd73a05c1eb8
skyhusker <skyhusker@fcc34d41-85d9-0310-a7b1-a7f9df185352>
2004-05-14 21:24:52 +0000

    Real main started

--- trunk/main.cc
+++ trunk/main.cc
@@ -1,23 +1,53 @@
 //$Id$
 
 #include <iostream>
+#include <getopt.h>
 #include "counter.h"
 #include "iface.h"
 
-using namespace std;
+void help()
+{
+  std::cout << "Netmeterd Version " << VERSION << std::endl
+  << "Copyright: Alberto Garcia Hierro and Ayoze Roberto "
+  << "Bernardo" << std::endl
+  << "Usage: netmeterd [options]" << std::endl
+  << "-i:    Specifies the interfaces (interface1,interface2,...)"
+  << std::endl
+  << "-t:    Specifies the time in secodns between counter updates"
+  << std::endl
+  << "-f:    Run on foreground (don't daemonize)" << std::endl;
 
-int main(int arg, char **argv){
+}
+
+int main(int argc, char *argv[])
+{
+  int time=1,ch;
+  bool wantsdaemon=true;
+  std::string interfaces;
   
-  iface a(argv[1]);
-  //a.load();
-  for (int i=0;i<atoi(argv[2]);i++)
-  {
-    sleep(1);
-    cout << a << endl;
-    a.update();
-  }
-  a.save();
-  cout << a << endl;
+
+  while ((ch = getopt (argc, argv, "i:t:fh")) != -1)
+    switch (ch)
+    {
+      case 'i':
+        interfaces = std::string(optarg);
+        break;
+      case 'f':
+        wantsdaemon=false;
+        break;
+      case 't':
+        time = atoi(optarg);
+        break;
+      case 'h':
+        help();
+        return 0;
+        break;
+      case '?':
+      default:
+        std::cout << "Try -h for more information." << std::endl;
+        return 1;
+      }
+
+  std::cout << time << " " << interfaces << std::endl;
   return 0;
-  
 }
