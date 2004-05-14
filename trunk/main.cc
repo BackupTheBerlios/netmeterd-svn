90e77001b94c4ec0c2200c78dfa8303efc32fe67
skyhusker <skyhusker@fcc34d41-85d9-0310-a7b1-a7f9df185352>
2004-05-14 17:31:33 +0000

    Sample

--- trunk/main.cc
+++ trunk/main.cc
@@ -8,18 +8,15 @@ using namespace std;
 
 int main(int arg, char **argv){
   
-  Method access;
-  access.type = FS;
-  access.data = "";
   iface a(argv[1]);
-  a.load(access);
+  //a.load();
   for (int i=0;i<atoi(argv[2]);i++)
   {
     sleep(1);
     cout << a << endl;
     a.update();
   }
-  a.save(access);
+  a.save();
   cout << a << endl;
   return 0;
   
