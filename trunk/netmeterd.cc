4ca8f2a0eacb7c9175fb2fabb35ad0d86068f26e
skyhusker <skyhusker@fcc34d41-85d9-0310-a7b1-a7f9df185352>
2004-05-15 02:12:41 +0000

    Added signal handlers and sanity checks

--- trunk/netmeterd.cc
+++ trunk/netmeterd.cc
@@ -1,10 +1,14 @@
 //$Id$
 
+
 #include <vector>
 #include <string>
 #include <iostream>
+#include <ctime>
 #include <getopt.h>
 #include <signal.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #include "counter.h"
 #include "iface.h"
@@ -15,10 +19,13 @@ void help()
   << "Copyright: Alberto Garcia Hierro and Ayoze Roberto "
   << "Bernardo" << std::endl
   << "Usage: netmeterd [options]" << std::endl
-  << "-i:   Specifies the interfaces (interface1,interface2,...)"
+  << "-i:   Interfaces to monitorize (interface1,interface2,...)"
   << std::endl
-  << "-t:   Specifies the time in secodns between counter updates"
+  << "-t:   Seconds between counter updates (default 1)"
   << std::endl
+  << "-T:   Minutes between saves to disk (default 5)" << std::endl
+  << "-l:   Specifies an alternate log dir " << std::endl
+  << "       (default " << LOGDIR << ")" << std::endl
   << "-f:   Run on foreground (don't daemonize)" << std::endl;
 
 }
@@ -43,23 +50,30 @@ std::vector <std::string> getNames (const std::string &interfaces)
   return ret;
 }
 
-void quit(){
-  std::cout << "Morire con dignidad" << std::endl;
+void quit(int sig){
+  std::cout << "Signal " << sig << " received" << std::endl;
+  std::cout << "Saving status of ifaces" << std::endl;
+  
+  std::cout << "Exiting..." << std::endl;
+  exit(1);
 }
 
 
 
 int main(int argc, char *argv[])
 {
-  int time=1,ch;
+  int updatetime=1,savetime=5,ch;
+  time_t prevsave=time(0);
   bool wantsdaemon=true;
-  std::string interfaces="";
+  extern char *optarg;
+  struct stat statBuf;
+  std::string logdir="",interfaces="";
   std::vector <iface> ifaces;
   std::vector <std::string> names; 
-  struct sigaction signal_action;
+  struct sigaction action;
 
   //Parse command line
-  while ((ch = getopt (argc, argv, "i:t:fh")) != -1)
+  while ((ch = getopt (argc, argv, "i:t:T:l:fh")) != -1)
     switch (ch)
     {
       case 'i':
@@ -69,7 +83,13 @@ int main(int argc, char *argv[])
         wantsdaemon=false;
         break;
       case 't':
-        time = atoi(optarg);
+        updatetime = atoi(optarg);
+        break;
+      case 'T':
+        savetime = atoi(optarg);
+        break;
+      case 'l':
+        logdir = std::string(optarg);
         break;
       case 'h':
         help();
@@ -89,23 +109,81 @@ int main(int argc, char *argv[])
   }
   
   names = getNames(interfaces);
+  
   //Create the ifaces
   for (unsigned int i=0;i<names.size();i++)
     ifaces.push_back(iface(names[i]));
+
+  //Set the logdir
+  if(logdir!="")
+    ifaces[0].setLogdir(logdir);
+    
   
-  //TODO: Check write perms
+  //Check write perms
+  if (stat(ifaces[0].getLogdir().c_str(), &statBuf) < 0)
+  {
+    std::cout << "Error: Cannot stat "
+    << ifaces[0].getLogdir().c_str() << std::endl
+    << "Exiting..." << std::endl;
+    return 1;
+  }
+
+  //Ugly conditional. Returns false if logdir 
+  //is not a writable directoriy
+  if (!S_ISDIR(statBuf.st_mode) || \
+    !(statBuf.st_mode & S_IWOTH || \
+    ( statBuf.st_mode & S_IWGRP && statBuf.st_gid == getgid()) || \
+    ( statBuf.st_mode & S_IWUSR && statBuf.st_uid == getuid()))) 
+  {
+    std::cout << "Error: " << ifaces[0].getLogdir().c_str()
+    << " should be a writable directory" << std::endl
+    << "Exiting..." << std::endl;
+    return 1;
+  }
+            
   
-  //Signal handlers
-  sigaddset(&signal_action.sa_mask, SIGTERM);
-  signal_action.sa_handler = quit;
-  sigaction(SIGTERM, &signal_action, 0);
-          
+  //Allright, so here we go :)
+  
+  //Load previous values, if any
+  for (unsigned int i=0;i<names.size();i++)
+    ifaces[i].load();
 
   
+  //Signal handlers
+  action.sa_handler = quit;
+  sigemptyset(&action.sa_mask);
+  action.sa_flags = SA_RESTART;
+  sigaction(SIGTERM, &action, NULL);
+  sigaction(SIGINT, &action, NULL);
+  
+  //Should I go background?
   if(wantsdaemon)
     daemon(0,0);
    
-  while (true);
-
+  //The main loop, a.k.a. the funny thing
+  while (true)
+  {  
+    //Update the ifaces
+    for (unsigned int i=0;i<names.size();i++)
+    {
+      ifaces[i].update();
+      //Some verbosity for foreground runs
+      std::cout << ifaces[i] << std::endl;
+    }
+    //If save time has passed, save the ifaces
+    if (time(0)-prevsave >= savetime*60)
+    {
+      for (unsigned int i=0;i<names.size();i++)
+      { 
+        ifaces[i].save();
+        //Some verbosity OMT
+        std::cout << "Saving state of " << ifaces[i].getName() << std::endl;
+      }
+      
+      prevsave=time(0);
+    }
+    //Wait for the next update
+    sleep(updatetime);
+  }
   return 0;
 }
