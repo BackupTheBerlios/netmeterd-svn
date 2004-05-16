d46fa4793d9aaf60eb35a6be1fd2af2469730023
skyhusker <skyhusker@fcc34d41-85d9-0310-a7b1-a7f9df185352>
2004-05-16 14:28:44 +0000

    We will use a counter class

--- trunk/scripts/nmstats.py
+++ trunk/scripts/nmstats.py
@@ -3,27 +3,63 @@
 #$Id$
 
 
-import sys,getopt,exceptions,time,os
+import getopt,exceptions,sys,os,time
+
+units = {0: 'B',1: 'KB',2: 'MB', 3: 'GB', \
+          4: 'TB', 5: 'PB', 6: 'HB', 'B': 0, \
+          'KB': 1, 'MB': 2, 'GB': 3, 'TB': 4,
+          'PB': 5, 'HB': 6 }
+
+
+class counter:
+  def __init__(self,count=0,unit=0):
+    self.count = count
+    try:
+      unit.isdigit()
+      self.unit = units[unit]
+    except:
+      self.unit = unit
+  def __str__(self):
+    return str(self.count)+' '+str(self.unit)
+  def __repr__(self):
+    return str(self.count)+' '+str(self.unit)
+  def __eq__(self,other):
+    if self.count == other.count and \
+        self.unit == other.unit: return True
+    return False
+  def __gt__(self,other):
+    if self.unit == other.unit:
+      return self.counter > other.counter
+    elif self.unit < other.unit:
+      return
+    
+
+
+
+
 
 def help():
-  revision = '$Rev$'
-  print 'nmstats revision', revision
-  print 'nmstats is part of netmeterd'
-  print 'Copytight: Alberto Garcia Hierro and Ayoze Roberto Bernardo'
+  revision = '$Rev$'[6:-2]
+  print os.path.basename(sys.argv[0]),'revision', revision
+  print os.path.basename(sys.argv[0]),'is part of netmeterd'
+  print 'Copyright: Alberto Garcia Hierro and Ayoze Roberto Bernardo'
   print 'Usage:',os.path.basename(sys.argv[0]),'[options]'
   print 'Available options are:'
-  print '-i:    Interfaces to generate stats (interface1,interface2,...)'
-  print '-l:    Netmeterd logdir (defaults to /var/log/netmeterd)'
-  print '-o:    Output this for the HTML (defaults to .)'
-  print '-d:    Date specification in the form day/month/year-day/month/year'
-  print '       Values "today", "month", and "year" are also accepted'
-  print '-t:    HTML template (defaults to "default")'
-  print '-T:    Template dir (defaults to /usr/share/netmeterd/templates/)'
-  print '-h:    Shows this help'
+  print ' -i:    Interfaces to generate stats (interface1,interface2,...)'
+  print ' -l:    Netmeterd logdir (defaults to /var/log/netmeterd)'
+  print ' -o:    Output this for the HTML (defaults to .)'
+  print ' -d:    Date specification in the form day/month/year-day/month/year'
+  print '        Values "today", "month", and "year" are also accepted'
+  print ' -t:    HTML template (defaults to "default")'
+  print ' -T:    Template dir (defaults to /usr/share/netmeterd/templates/)'
+  print ' -h:    Shows this help'
+  print ''
   pass
 
 
 def isAfter(refDay,refMonth,refYear,day,month,year):
+  """Returns True if the date specified is after
+  the reference date, False otherwise."""
   if year > refYear: return True
   elif year < refYear: return False
   else:
@@ -36,7 +72,10 @@ def isAfter(refDay,refMonth,refYear,day,month,year):
 
 
 def getData(date,interfaces,logdir):
-  
+  """Parses files in logdir, recollecting data that
+  matchs the specified timespec and the selected
+  interfaces. Returns a dict containing all the info""" 
+
   #Init the dict
   ifaces = {}
   buf = ""
@@ -67,9 +106,7 @@ def getData(date,interfaces,logdir):
     if date == 'month':
       stDay = 1
       endDay = 31
-    elif date == 'year':
-      stDay = 1
-      endDay = 31
+    if date == 'year':
       stMonth = 1
       endMonth = 12
             
@@ -101,7 +138,37 @@ def getData(date,interfaces,logdir):
   return ifaces
   pass
 
-def createHTML(ifaces,templatedir,template,outdir):
+def expandVal(pair):
+  val = float(pair[0])
+   
+  if pair[1] == 'B': pow=-2
+  elif pair[1] == 'KB': pow=-1
+  elif pair[1] == 'MB': pow=0
+  elif pair[1] == 'GB': pow=1
+  elif pair[1] == 'TB': pow=2
+  elif pair[1] == 'PB': pow=3
+  elif pair[1] == 'HB': pow=4
+  val = val * (1024**pow)
+  
+  return val
+  
+def getTheBiggest(ifaces):
+  value = 0
+  for iface in ifaces.keys():
+    for date in ifaces[iface].keys():
+      tmpval = expandVal(ifaces[iface][date][:2])
+      if tmpval > value: value=tmpval
+      tmpval = expandVal(ifaces[iface][date][2:]) 
+      if tmpval > value: value=tmpval
+  return value
+      
+def createHTML(ifaces,templatedir,template,outdir,type):
+  
+  
+  
+  
+  max = getTheBiggest(ifaces)
+  print max
   pass
 
 def main():
@@ -113,7 +180,7 @@ def main():
   template='default'
   templatedir='/usr/share/netmeterd/templates/'
   outdir='.'
-  
+  type = 'D'
   #Parse command line
   try:
     options, notUsed = getopt.getopt(sys.argv[1:],'d:l:i:t:T:','help')
@@ -138,6 +205,7 @@ def main():
 
   ifaces = getData(date,interfaces,logdir)
   print ifaces
+  createHTML(ifaces,templatedir,template,outdir,type)
 
 if __name__ == '__main__' :
   main()
