2cf81d7d19becf12ef0ea15946ebe3144985d043
skyhusker <skyhusker@fcc34d41-85d9-0310-a7b1-a7f9df185352>
2004-05-17 01:05:04 +0000

    Added clean functionality

--- trunk/autogen.sh
+++ trunk/autogen.sh
@@ -1,3 +1,19 @@
-aclocal
-autoconf
-automake -a
+#!/bin/sh
+if test -z "$1"
+then
+  aclocal
+  autoconf
+  automake -a
+else
+  if test "$1" = "clean"
+  then
+    make clean
+    make maintainer-clean-am
+    rm -fr src/Makefile src/Makefile.in \
+          Makefile Makefile.in \
+          aclocal.m4 mkinstalldirs install-sh \
+         missing config.status configure \
+         src/.deps
+  fi
+fi
+
